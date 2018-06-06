e1a8ff976a924951dd7dc25b52e40af7c110b681
Ryan McBride <ryanmcbride@Ryans-MacBook-Pro.local>
2018-06-05 20:19:41 -0600

    more endpoints

--- main.go
+++ main.go
@@ -58,15 +58,42 @@ func main() {
 	}
 	alexa.Run(Applications, port)
 }
+func EchoBalanceIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	echoResp.OutputSpeech("Balance handler").Card("Hello World", "Balance")
+}
+func EchoNumberIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	echoResp.OutputSpeech("Number handler").Card("Hello World", "Number")
+}
+func EchoNetworthIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	echoResp.OutputSpeech("Networth handler").Card("Hello World", "Networth")
+}
+func EchoSpendingIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	echoResp.OutputSpeech("Spending handler").Card("Hello World", "Spending")
+}
 func EchoIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
 	echoResp.OutputSpeech("Why is Connor super cool!").Card("Hello World", "This is a test card.")
 }
 
 var Applications = map[string]interface{}{
-	"/echo/hello": alexa.EchoApplication{ // Route
+	"/echo/balance": alexa.EchoApplication{ // Route
 		AppID:    "amzn1.ask.skill.36681e9c-17d6-4051-8f12-30ba2328e619", // Echo App ID from Amazon Dashboard
-		OnIntent: EchoIntentHandler,
-		OnLaunch: EchoIntentHandler,
+		OnIntent: EchoBalanceIntentHandler,
+		OnLaunch: EchoBalanceIntentHandler,
+	},
+	"/echo/number": alexa.EchoApplication{ // Route
+		AppID:    "amzn1.ask.skill.36681e9c-17d6-4051-8f12-30ba2328e619", // Echo App ID from Amazon Dashboard
+		OnIntent: EchoNumberIntentHandler,
+		OnLaunch: EchoNumberIntentHandler,
+	},
+	"/echo/networth": alexa.EchoApplication{ // Route
+		AppID:    "amzn1.ask.skill.36681e9c-17d6-4051-8f12-30ba2328e619", // Echo App ID from Amazon Dashboard
+		OnIntent: EchoNetworthIntentHandler,
+		OnLaunch: EchoNetworthIntentHandler,
+	},
+	"/echo/spending": alexa.EchoApplication{ // Route
+		AppID:    "amzn1.ask.skill.36681e9c-17d6-4051-8f12-30ba2328e619", // Echo App ID from Amazon Dashboard
+		OnIntent: EchoSpendingIntentHandler,
+		OnLaunch: EchoSpendingIntentHandler,
 	},
 	"/echo/": alexa.EchoApplication{ // Route
 		AppID:    "amzn1.ask.skill.36681e9c-17d6-4051-8f12-30ba2328e619", // Echo App ID from Amazon Dashboard
